703835777503d2b5dcb301cf92d7d1cae0f686fa
Linh Dao <linhdh@live.com>
2024-01-18 16:44:37 +0700

    add entity framework part 1.

---
+++ be/GoldPriceMonitorApi_DotNet/GoldPriceMonitorApi_DotNet/Database/GoldPriceDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GoldPriceMonitorApi_DotNet.Database
+{
+    public class GoldPriceDbContext : DbContext
+    {
+        public GoldPriceDbContext(DbContextOptions<GoldPriceDbContext> options) : base(options) { }
+
+        public DbSet<BaoTinMinhChau> BaoTinMinhChaus { get; set; }
+
+    }
+}
