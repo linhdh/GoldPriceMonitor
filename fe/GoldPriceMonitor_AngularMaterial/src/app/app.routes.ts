1394742c6941bee320094bcedac5f017b8dca3d2
Linh Dao <linhdh@live.com>
2024-01-26 21:30:44 +0700

    add route to BaoTinMinhChauComponent.

--- fe/GoldPriceMonitor_AngularMaterial/src/app/app.routes.ts
+++ fe/GoldPriceMonitor_AngularMaterial/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { BaoTinMinhChauComponent } from './bao-tin-minh-chau/bao-tin-minh-chau.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', 
+        component: BaoTinMinhChauComponent, 
+        title: 'Bảo tín minh châu'
+    }
+];
---
+++ fe/GoldPriceMonitor_AngularMaterial/src/app/bao-tin-minh-chau/bao-tin-minh-chau.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bao-tin-minh-chau',
+  standalone: true,
+  imports: [],
+  templateUrl: './bao-tin-minh-chau.component.html',
+  styleUrl: './bao-tin-minh-chau.component.css'
+})
+export class BaoTinMinhChauComponent {
+
+}
