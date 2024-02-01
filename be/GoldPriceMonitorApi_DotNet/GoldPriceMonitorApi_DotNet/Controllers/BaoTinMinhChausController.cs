9adc01314841b1b752f0cc464e7ea13670423179
Linh Dao <linhdh@live.com>
2024-02-01 07:07:22 +0700

    Create BaoTinMinhChau graph part 1.

---
+++ be/GoldPriceMonitorApi_DotNet/GoldPriceMonitorApi_DotNet/Controllers/BaoTinMinhChausController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GoldPriceMonitorApi_DotNet.Database;
+using GoldPriceMonitorApi_DotNet.Controllers.Parameters.BaoTinMinhChausController;
+
+namespace GoldPriceMonitorApi_DotNet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaoTinMinhChausController : ControllerBase
+    {
+        private readonly GoldPriceDbContext _context;
+
+        public BaoTinMinhChausController(GoldPriceDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/BaoTinMinhChaus
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<GoldType>>> GetNames()
+        {
+            return await _context.BaoTinMinhChaus.GroupBy(b => new { b.Name, b.HamLuongKara, b.HamLuongVang }).Select(b => new GoldType() { 
+                Name = b.Key.Name, 
+                HamLuongKara = b.Key.HamLuongKara, 
+                HamLuongVang = b.Key.HamLuongVang 
+            }).ToListAsync();
+        }
+
+        [HttpGet("ByType")]
+        public async Task<ActionResult<IEnumerable<BaoTinMinhChau>>> GetBaoTinMinhChauToday([FromQuery] GoldType type)
+        {
+            var baoTinMinhChau = await _context.BaoTinMinhChaus.Where(b => b.Name == type.Name && b.HamLuongVang == type.HamLuongVang && b.HamLuongKara == type.HamLuongKara && b.ThoiGianNhap.Date == DateTime.Now.Date).Select(b => new BaoTinMinhChau()
+            {
+                Id = b.Id, 
+                Name = b.Name, 
+                HamLuongVang = b.HamLuongVang, 
+                HamLuongKara = b.HamLuongKara, 
+                GiaBanRa = b.GiaBanRa, 
+                GiaMuaVao = b.GiaMuaVao, 
+                GiaTheGioi = b.GiaTheGioi, 
+                ThoiGianNhap = b.ThoiGianNhap
+            }).ToListAsync();
+
+            if (baoTinMinhChau.Any())
+            {
+                return baoTinMinhChau;
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ be/GoldPriceMonitorApi_DotNet/GoldPriceMonitorApi_DotNet/Controllers/Parameters/BaoTinMinhChausController/GoldType.cs
@@ -0,0 +1,9 @@
+﻿namespace GoldPriceMonitorApi_DotNet.Controllers.Parameters.BaoTinMinhChausController
+{
+    public class GoldType
+    {
+        public string Name { get; set; }
+        public string HamLuongKara { get; set; }
+        public string HamLuongVang { get; set; }
+    }
+}
