8bb1e429d9f5dcfe004b586e54a3a482c5b58b1c
Linh Dao <linhdh@live.com>
2024-03-17 00:01:09 +0700

    add new routing routes.

--- fe/GoldPriceMonitor_AngularMaterial/src/app/app.component.ts
+++ fe/GoldPriceMonitor_AngularMaterial/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
@@ -7,7 +7,7 @@ import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ RouterOutlet, MatToolbarModule, MatButtonModule ],
+  imports: [ RouterOutlet, MatToolbarModule, MatButtonModule, RouterModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- fe/GoldPriceMonitor_AngularMaterial/src/app/app.routes.ts
+++ fe/GoldPriceMonitor_AngularMaterial/src/app/app.routes.ts
@@ -1,10 +1,21 @@
 import { Routes } from '@angular/router';
 import { BaoTinMinhChauComponent } from './bao-tin-minh-chau/bao-tin-minh-chau.component';
+import { SjcComponent } from './sjc/sjc.component';
 
 export const routes: Routes = [
-    {
+    { 
         path: '', 
+        redirectTo: 'bao-tin-minh-chau', 
+        pathMatch: 'full' 
+    },
+    {
+        path: 'bao-tin-minh-chau', 
         component: BaoTinMinhChauComponent, 
+        title: 'Bảo tín minh châu'
+    }, 
+    {
+        path: 'sjc', 
+        component: SjcComponent, 
         title: 'Bảo tín minh châu'
     }
 ];
