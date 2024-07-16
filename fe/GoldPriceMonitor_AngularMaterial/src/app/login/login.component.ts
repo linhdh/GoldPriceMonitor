6cd681fb2b0eddbca2addce223d17a1c7849db12
Linh Dao <linhdh@live.com>
2024-07-16 17:47:02 +0700

    create a basic login form.

--- fe/GoldPriceMonitor_AngularMaterial/src/app/login/login.component.ts
+++ fe/GoldPriceMonitor_AngularMaterial/src/app/login/login.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCommonModule } from '@angular/material/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ MatFormFieldModule, 
+    MatCheckboxModule, 
+    ReactiveFormsModule, 
+    MatButtonModule, 
+    MatInputModule, 
+    MatCommonModule, 
+    CommonModule, 
+    MatCardModule, 
+    MatDividerModule,
+    FormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
 }
