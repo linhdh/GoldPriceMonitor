e792f0165cf1c9ee1a62739400f00187bec455a1
Linh Dao <linhdh@live.com>
2024-07-16 18:36:15 +0700

    simple sign up html code.

--- fe/GoldPriceMonitor_AngularMaterial/src/app/sign-up/sign-up.component.ts
+++ fe/GoldPriceMonitor_AngularMaterial/src/app/sign-up/sign-up.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [ MatCardModule, MatInputModule, MatFormFieldModule, MatButtonModule ],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
