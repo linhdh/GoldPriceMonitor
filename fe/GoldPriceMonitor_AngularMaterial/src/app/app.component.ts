b7218d759644dc6316f4fe656d2d5abad3a39daa
Linh Dao <linhdh@live.com>
2024-08-07 16:54:38 +0700

    fix text color for sign-in and sign-up components. Fix background color.

--- fe/GoldPriceMonitor_AngularMaterial/src/app/app.component.ts
+++ fe/GoldPriceMonitor_AngularMaterial/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 
 @Component({
